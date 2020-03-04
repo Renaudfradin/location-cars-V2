67125b2ead61efaaa186cb01f7371b0acc416e11
Renaud <48203798+Renaudfradin@users.noreply.github.com>
2020-03-04 23:27:39 +0100

    Update js.js

--- js/js.js
+++ js/js.js
@@ -22,4 +22,89 @@ document.addEventListener('DOMContentLoaded', () => {
     });
     }
 
-    });
+});
+function controlpseudo(champ) {
+    if (champ.value.length > 2 && champ.value.length < 50) {
+        console.log("bon");
+        document.getElementById("pseudo-inscription").className = "input is-success";
+    }
+    else if(champ.value.length == 0){
+        console.log("pa bon");
+        document.getElementById("pseudo-inscription").className = "input";
+    }
+    else{
+        console.log("pa bon");
+        document.getElementById("pseudo-inscription").className = "input is-danger";
+    }
+};
+function controlnom(champ) {
+    if (champ.value.length > 2 && champ.value.length < 50) {
+        //const notiferreurs = document.getElementById("nom-inscription-erreur");
+        console.log("bon");
+        document.getElementById("nom-inscription").className = "input is-success";
+    }
+    else if(champ.value.length == 0){
+        console.log("pa bon");
+        document.getElementById("nom-inscription").className = "input";
+    }
+    else{
+        console.log("pa bon");
+        document.getElementById("nom-inscription").className = "input is-danger";
+        //document.getElementById("nom-inscription-erreur").innerHTML += "<p class='help is-success'>This usernasqqssqsqsqsqsqsqsqme is available</p>";
+    }
+};
+function controlprenom(champ) {
+    if (champ.value.length > 2 && champ.value.length < 50) {
+        console.log("bon");
+        document.getElementById("prenom-inscription").className = "input is-success";
+    }
+    else if(champ.value.length == 0){
+        console.log("pa bon");
+        document.getElementById("prenom-inscription").className = "input";
+    }
+    else{
+        console.log("pa bon");
+        document.getElementById("prenom-inscription").className = "input is-danger";
+    }
+};
+function controlmdp(champ) {
+    if (champ.value.length > 2 && champ.value.length < 50) {
+        console.log("bon");
+        document.getElementById("mdp-inscription").className = "input is-success";
+    }
+    else if(champ.value.length == 0){
+        console.log("pa bon");
+        document.getElementById("mdp-inscription").className = "input";
+    }
+    else{
+        console.log("pa bon");
+        document.getElementById("mdp-inscription").className = "input is-danger";
+    }
+};
+function controlmdp1(champ) {
+    if (champ.value.length > 2 && champ.value.length < 50) {
+        console.log("bon");
+    }
+    else if(champ.value.length == 0){
+        console.log("pa bon");
+        document.getElementById("mpd1-inscription").className = "input";
+    }
+    else{
+        console.log("pa bon");
+        document.getElementById("mdp1-inscription").className = "input is-danger";
+    }
+};
+function controlle2mdp() {
+    const mdp1 = document.getElementById("mdp-inscription").value;
+    const mdp2 = document.getElementById("mdp1-inscription").value;
+    console.log(mdp1);
+    console.log(mdp2);
+    if (mdp1 == mdp2){
+        console.log("mdp egale");
+        document.getElementById("mdp1-inscription").className = "input is-success";
+    }
+    else{
+        console.log("mdp pa egale");
+        document.getElementById("mdp1-inscription").className = "input is-danger";
+    }
+};

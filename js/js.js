89f94f87b72bc556767fbb969ed75d3273925ada
Renaud <48203798+Renaudfradin@users.noreply.github.com>
2020-03-06 00:59:01 +0100

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -28,17 +28,31 @@ function regexcontrollettre() {
     return regex;
 };
 function regexcontrolmail() {
-    const regex1 = /^[^\W][a-zA-Z0-9](\.[a-zA-Z0-9_]+)*\@[a-zA-Z]+(\.[a-zA-Z0-9_]+)*\.[a-z]{2,4}$/;
+    const regex1 = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     return regex1;
 };
+function isdanger() {
+    const isdanger = "input is-danger";
+    return isdanger;
+};
+function issucces() {
+    const issucces = "input is-success";
+    return issucces;
+};
+function normalinput() {
+    const normalinput = "input";
+    return normalinput;
+};
+
+
 function controlmail(champ) {
     const regex1 = regexcontrolmail();
     if (regex1.test(champ.value)) {
         console.log("respect email");
-        document.getElementById("email-inscription").className = "input is-success";
+        document.getElementById("email-inscription").className = issucces();
     }else{
         console.log("pa respect mail");
-        document.getElementById("email-inscription").className = "input is-danger";
+        document.getElementById("email-inscription").className = isdanger();
 
     }
 };
@@ -48,20 +62,20 @@ function controlpseudo(champ) {
         if (regex.test(champ.value)) {
             console.log("respect lettre");
             console.log("bon");
-            document.getElementById("pseudo-inscription").className = "input is-success";
+            document.getElementById("pseudo-inscription").className = issucces();
         }
         else{
             console.log("pa bon");
-            document.getElementById("pseudo-inscription").className = "input is-danger";
+            document.getElementById("pseudo-inscription").className = isdanger();
         }
     }
     else if(champ.value.length == 0){
         console.log("pa bon");
-        document.getElementById("pseudo-inscription").className = "input";
+        document.getElementById("pseudo-inscription").className = normalinput();
     }
     else{
         console.log("pa bon");
-        document.getElementById("pseudo-inscription").className = "input is-danger";
+        document.getElementById("pseudo-inscription").className = isdanger();
     }
 };
 function controlnom(champ) {
@@ -70,21 +84,21 @@ function controlnom(champ) {
         if (regex.test(champ.value)) {
             console.log("respect lettre");
             console.log("bon");
-            document.getElementById("nom-inscription").className = "input is-success";
+            document.getElementById("nom-inscription").className = issucces();
         }
         else{
             console.log("pa bon");
-            document.getElementById("nom-inscription").className = "input is-danger";
+            document.getElementById("nom-inscription").className = isdanger();
         }
         //const notiferreurs = document.getElementById("nom-inscription-erreur");
     }
     else if(champ.value.length == 0){
         console.log("pa bon");
-        document.getElementById("nom-inscription").className = "input";
+        document.getElementById("nom-inscription").className = normalinput();
     }
     else{
         console.log("pa bon");
-        document.getElementById("nom-inscription").className = "input is-danger";
+        document.getElementById("nom-inscription").className = isdanger();
         //document.getElementById("nom-inscription-erreur").innerHTML += "<p class='help is-success'>This usernasqqssqsqsqsqsqsqsqme is available</p>";
     }
 };
@@ -93,62 +107,72 @@ function controlprenom(champ) {
         if (regex.test(champ.value)) {
             console.log("respect lettre");
             console.log("bon");
-            document.getElementById("prenom-inscription").className = "input is-success";
+            document.getElementById("prenom-inscription").className = issucces();
         }
         else{
             console.log("pa bon");
-            document.getElementById("prenom-inscription").className = "input is-danger";
+            document.getElementById("prenom-inscription").className = isdanger();
         }
     }
     else if(champ.value.length == 0){
         console.log("pa bon");
-        document.getElementById("prenom-inscription").className = "input";
+        document.getElementById("prenom-inscription").className = normalinput();
     }
     else{
         console.log("pa bon");
-        document.getElementById("prenom-inscription").className = "input is-danger";
+        document.getElementById("prenom-inscription").className = isdanger();
     }
 };
 function controlmdp(champ) {
     if (champ.value.length > 2 && champ.value.length < 50) {
         console.log("bon");
-        document.getElementById("mdp-inscription").className = "input is-success";
+        document.getElementById("mdp-inscription").className = issucces();
     }
     else if(champ.value.length == 0){
         console.log("pa bon");
-        document.getElementById("mdp-inscription").className = "input";
+        document.getElementById("mdp-inscription").className = normalinput();
     }
     else{
         console.log("pa bon");
-        document.getElementById("mdp-inscription").className = "input is-danger";
+        document.getElementById("mdp-inscription").className = isdanger();
     }
 };
 function controlmdp1(champ) {
     if (champ.value.length > 2 && champ.value.length < 50) {
         console.log("bon");
-        document.getElementById("mdp1-inscription").className = "input is-success";
+        document.getElementById("mdp1-inscription").className = issucces();
     }
     else if(champ.value.length == 0){
         console.log("pa bon");
-        document.getElementById("mpd1-inscription").className = "input";
+        document.getElementById("mpd1-inscription").className = normalinput();
     }
     else{
         console.log("pa bon");
-        document.getElementById("mdp1-inscription").className = "input is-danger";
+        document.getElementById("mdp1-inscription").className = isdanger();
     }
 };
 function controlleinfo() {
     const mdp1 = document.getElementById("mdp-inscription").value;
     const mdp2 = document.getElementById("mdp1-inscription").value;
-    console.log(mdp1);
-    console.log(mdp2);
+    const prenom = ;  
+    const nom = ;
+    const pseudo = ;
+    const email = ;
+    //console.log(mdp1);
+    //console.log(mdp2);
+   
     if (mdp1 == mdp2){
-        console.log("mdp egale");
-        document.getElementById("mdp1-inscription").className = "input is-success";
+        if (mdp1.length === 0 || mdp2 === 0) {
+            document.getElementById("mdp-inscription").className = isdanger();
+            document.getElementById("mdp1-inscription").className = isdanger();
+        }else{
+            console.log("mdp egale");
+            document.getElementById("mdp1-inscription").className = issucces();
+        }
     }
     else{
         console.log("mdp pa egale");
-        document.getElementById("mdp-inscription").className = "input is-danger";
-        document.getElementById("mdp1-inscription").className = "input is-danger";
+        document.getElementById("mdp-inscription").className = isdanger();
+        document.getElementById("mdp1-inscription").className = isdanger();
     }
 };
